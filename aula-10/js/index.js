19008aef3f93dc8c7297bd2cd90eefe9d509393e
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-11-18 07:56:35 -0300

    Refactor code to make code easy to add new fields

--- aula-10/js/index.js
+++ aula-10/js/index.js
@@ -2,64 +2,48 @@ let editRowIndex = -1
 
 const start = () => {
     const saveBtn = document.getElementById('saveBtn')
-    saveBtn.onclick = saveStudent
+    saveBtn.onclick = saveBtnEvent
 }
 
-const saveStudent = () => {
-    const inputName = document.getElementById('name')
-    const inputEmail = document.getElementById('email')
-    const inputCellPhone = document.getElementById('cellphone')
+const saveBtnEvent = () => {
+    const inputArray = createArrayWithInputs()
 
     if (editRowIndex === -1) { 
-        const tdName = document.createElement('td')
-        const tdEmail = document.createElement('td')
-        const tdCellPhone = document.createElement('td')
-        const tdUpdate = document.createElement('td')
-        const tdRemove = document.createElement('td')
-
-        const tdNameContent = document.createTextNode(inputName.value)
-        const tdEmailContent = document.createTextNode(inputEmail.value)
-        const tdCellPhoneContent = document.createTextNode(inputCellPhone.value)
-        const updateButton = document.createElement('input')
-        updateButton.type = 'button'
-        updateButton.value = 'Editar'
-        updateButton.onclick = editStudent
-        const removeButton = document.createElement('input')
-        removeButton.type = 'button'
-        removeButton.value = 'Remover'
-        removeButton.onclick = removeStudent
-
-        tdName.appendChild(tdNameContent)
-        tdEmail.appendChild(tdEmailContent)
-        tdCellPhone.appendChild(tdCellPhoneContent)
-        tdUpdate.appendChild(updateButton)
-        tdRemove.appendChild(removeButton)
-        
-        const newLine = document.createElement('tr')
-        newLine.appendChild(tdName)
-        newLine.appendChild(tdEmail)
-        newLine.appendChild(tdCellPhone)
-        newLine.appendChild(tdUpdate)
-        newLine.appendChild(tdRemove)
-
-        const table = document.getElementById('studentTable')
-        const tBody = table.tBodies[0]
-        tBody.appendChild(newLine)
+        saveNewStudent(inputArray)
     } else {
-        const table = document.getElementById('studentTable')
-        const tBody = table.tBodies[0]
-        const editRow = tBody.childNodes[editRowIndex - 1]
-
-        editRow.childNodes[0].innerHTML = inputName.value
-        editRow.childNodes[1].innerHTML = inputEmail.value
-        editRow.childNodes[2].innerHTML = inputCellPhone.value
-
-        editRowIndex = -1
+        saveStudentAlreadyExist(inputArray)
     }
 
-    inputName.value = ''
-    inputCellPhone.value = ''
-    inputEmail.value = ''
+    inputArray.forEach(aux => aux.value = '')
+}
+
+const saveNewStudent = inputArray => {
+    const newLine = document.createElement('tr')
+    inputArray.forEach(input => {
+        const td = document.createElement('td')
+        const tdContent = document.createTextNode(input.value)
+        td.appendChild(tdContent)
+        newLine.appendChild(td)
+    })
+
+    createActionButton('Editar', editStudent, newLine)
+    createActionButton('Remover', removeStudent, newLine)
+
+    const table = document.getElementById('studentTable')
+    const tBody = table.tBodies[0]
+    tBody.appendChild(newLine)
+}
+
+const saveStudentAlreadyExist = inputArray => {
+    const table = document.getElementById('studentTable')
+    const tBody = table.tBodies[0]
+    const editRow = tBody.childNodes[editRowIndex - 1]
+
+    inputArray.forEach((input, index) => {
+        editRow.childNodes[index].innerHTML = input.value    
+    })
+
+    editRowIndex = -1
 }
 
 const editStudent = event => {
@@ -68,13 +52,10 @@ const editStudent = event => {
     const tr = td.parentNode
     editRowIndex = tr.rowIndex
 
-    const inputName = document.getElementById('name')
-    const inputEmail = document.getElementById('email')
-    const inputCellPhone = document.getElementById('cellphone')
-
-    inputName.value = tr.childNodes[0].innerHTML
-    inputEmail.value = tr.childNodes[1].innerHTML
-    inputCellPhone.value = tr.childNodes[2].innerHTML
+    const inputArray = createArrayWithInputs()
+    inputArray.forEach((input, index) => {
+        input.value = tr.childNodes[index].innerHTML
+    })
 }
 
 const removeStudent = event => {
@@ -83,6 +64,24 @@ const removeStudent = event => {
     const tr = td.parentNode
     const tBody = tr.parentNode
     tBody.removeChild(tr)
+}
+
+const createArrayWithInputs = () => {
+    const inputName = document.getElementById('name')
+    const inputEmail = document.getElementById('email')
+    const inputCellPhone = document.getElementById('cellphone')
+    const inputBirthday = document.getElementById('birthday')
+    return [inputName, inputEmail, inputCellPhone, inputBirthday]
+}
+
+const createActionButton = (inputValue, clickEventCallback, newLine) => {
+    const td = document.createElement('td')
+    button = document.createElement('input')
+    button.type = 'button'
+    button.value = inputValue
+    button.onclick = clickEventCallback
+    td.appendChild(button)
+    newLine.appendChild(td)
 }
 
 start()
