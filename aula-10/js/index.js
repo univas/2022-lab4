5884893161780b6e781431ebe470dc86def86133
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-11-30 19:56:37 -0300

    Update aula-10 to use LocalStorage

--- aula-10/js/index.js
+++ aula-10/js/index.js
@@ -1,56 +1,56 @@
-let editRowIndex = -1
+let studentId = null
+let studentList = []
 
 const start = () => {
+    loadLocalStorageItems()
+
     const saveBtn = document.getElementById('saveBtn')
     saveBtn.onclick = saveBtnEvent
 }
 
 const saveBtnEvent = () => {
     const inputArray = createArrayWithInputs()
+    const student = createStudentObject(inputArray)
 
-    if (editRowIndex === -1) { 
-        saveNewStudent(inputArray)
+    if (!studentId) {
+        student.id = (new Date()).getTime()
+        studentList.push(student)
     } else {
-        saveStudentAlreadyExist(inputArray)
+        const index = studentList.findIndex(student => student.id == studentId)
+        studentList[index].name = student.name
+        studentList[index].email = student.email
+        studentList[index].cellphone = student.cellphone
+        studentList[index].birthday = student.birthday
     }
 
+    mountTable()
     inputArray.forEach(aux => aux.value = '')
+    saveLocalStorage()
+    studentId = null
 }
 
-const saveNewStudent = inputArray => {
-    const newLine = document.createElement('tr')
-    inputArray.forEach(input => {
-        const td = document.createElement('td')
-        const tdContent = document.createTextNode(input.value)
-        td.appendChild(tdContent)
-        newLine.appendChild(td)
-    })
-
-    createActionButton('Editar', editStudent, newLine)
-    createActionButton('Remover', removeStudent, newLine)
-
+const mountTable = () => {
     const table = document.getElementById('studentTable')
     const tBody = table.tBodies[0]
-    tBody.appendChild(newLine)
-}
-
-const saveStudentAlreadyExist = inputArray => {
-    const table = document.getElementById('studentTable')
-    const tBody = table.tBodies[0]
-    const editRow = tBody.childNodes[editRowIndex - 1]
-
-    inputArray.forEach((input, index) => {
-        editRow.childNodes[index].innerHTML = input.value    
+    tBody.innerHTML = ''
+    
+    studentList.forEach(student => {
+        const newLine = document.createElement('tr')
+        createTableData(newLine, student.name)
+        createTableData(newLine, student.email)
+        createTableData(newLine, student.cellphone)
+        createTableData(newLine, student.birthday)
+        createActionButton('Editar', editStudent, newLine, student.id)
+        createActionButton('Remover', removeStudent, newLine, student.id)
+        tBody.appendChild(newLine)
     })
-
-    editRowIndex = -1
 }
 
 const editStudent = event => {
     const editBtn = event.target
     const td = editBtn.parentNode
     const tr = td.parentNode
-    editRowIndex = tr.rowIndex
+    studentId = editBtn.id
 
     const inputArray = createArrayWithInputs()
     inputArray.forEach((input, index) => {
@@ -60,10 +60,12 @@ const editStudent = event => {
 
 const removeStudent = event => {
     const removeBtn = event.target
-    const td = removeBtn.parentNode
-    const tr = td.parentNode
-    const tBody = tr.parentNode
-    tBody.removeChild(tr)
+
+    const index = studentList.findIndex(student => student.id == removeBtn.id)
+    console.log(index)
+    studentList.splice(index, 1)
+    saveLocalStorage()
+    mountTable()
 }
 
 const createArrayWithInputs = () => {
@@ -74,14 +76,43 @@ const createArrayWithInputs = () => {
     return [inputName, inputEmail, inputCellPhone, inputBirthday]
 }
 
-const createActionButton = (inputValue, clickEventCallback, newLine) => {
+const createActionButton = (inputValue, clickEventCallback, newLine, id) => {
     const td = document.createElement('td')
     button = document.createElement('input')
     button.type = 'button'
     button.value = inputValue
     button.onclick = clickEventCallback
+    button.id = id
     td.appendChild(button)
     newLine.appendChild(td)
+}
+
+const createStudentObject = inputArray => {
+    return {
+        name: inputArray[0].value,
+        email: inputArray[1].value,
+        cellphone: inputArray[2].value,
+        birthday:  inputArray[3].value
+    }
+}
+
+const createTableData = (newLine, inputValue) => {
+    const td = document.createElement('td')
+    const tdContent = document.createTextNode(inputValue)
+    td.appendChild(tdContent)
+    newLine.appendChild(td)
+}
+
+const saveLocalStorage = () => {
+    localStorage.setItem('STUDENTS', JSON.stringify(studentList))
+}
+
+const loadLocalStorageItems = () => {
+    const items = localStorage.getItem('STUDENTS')
+    if (items) {
+        studentList = JSON.parse(items)
+        mountTable()
+    }
 }
 
 start()
