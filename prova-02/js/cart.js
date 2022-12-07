9e14f738b23b47a231771bb5457944a6d1df3dc8
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-12-07 15:22:26 -0300

    Add prova-02

--- prova-02/js/cart.js
+++ prova-02/js/cart.js
@@ -1,1 +1,25 @@
-console.log('implements')
+
+
+const LOCAL_KEY = 'my-chart'
+
+const start = () => {
+    updateChartItems()
+}
+
+const updateChartItems = () => {
+    const items = localStorage.getItem(LOCAL_KEY)
+    if (items) {
+        const books = JSON.parse(items)
+        document.getElementById('quantity').innerHTML = books.length
+        updateItems(books)
+    }
+}
+
+const updateItems = books => {
+    books.forEach(bookId => {
+        const span = document.getElementById(bookId + 'Quantity')
+        span.innerHTML = parseInt(span.innerHTML, 10) + 1
+    })
+}
+
+start()
--- prova-02/js/index.js
+++ prova-02/js/index.js
@@ -1,1 +1,37 @@
-console.log('implements')
+const LOCAL_KEY = 'my-chart'
+
+const start = () => {
+    const buyButtons = document.getElementsByClassName('buyButton')
+    Array.from(buyButtons).forEach(button => button.onclick = buyEventClick)
+
+    updateChartItems()
+}
+
+const buyEventClick = event => {
+    const button = event.target
+    const img = button.parentNode.children[0]
+    const items = localStorage.getItem(LOCAL_KEY)
+    let quantity = 1
+    if (!items) {
+        const books = [img.id]
+        localStorage.setItem(LOCAL_KEY, JSON.stringify(books))
+
+    } else {
+        const books = JSON.parse(items)
+        books.push(img.id)
+        quantity = books.length
+        localStorage.setItem(LOCAL_KEY, JSON.stringify(books))
+    }
+
+    document.getElementById('quantity').innerHTML = quantity
+}
+
+const updateChartItems = () => {
+    const items = localStorage.getItem(LOCAL_KEY)
+    if (items) {
+        const books = JSON.parse(items)
+        document.getElementById('quantity').innerHTML = books.length
+    }
+}
+
+start()
